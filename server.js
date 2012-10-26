cd7b8d3618eb34b27a084b4bcae0cca5e97575ed
Nathan Friedly <nathan@nfriedly.com>
2012-10-26 10:29:59 -0700

    checking the FB post data

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 app.all('*', function(req, res) {
+	res.on('data', function(chunk) {
+		console.log(chunk.toString());
+	});
 	res.redirect('https://s3.amazonaws.com/static.extendedposts.com/test.html');
 });
 
